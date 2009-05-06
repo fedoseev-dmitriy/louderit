6aa5986692ae43ab9e727634de064aebf175fc7a
turbidit <turbidit@9372757c-d666-11dd-9aad-3f7dfcff7c77>
2009-05-06 21:44:02 +0000

    tray context menu moved to resource

--- trunk/louderit/inc/resource.h
+++ trunk/louderit/inc/resource.h
@@ -4,13 +4,19 @@
 //
 #define IDI_ICON1                       101
 #define IDI_ICON2                       103
+#define IDR_MENU1                       104
+#define IDR_CONTEXT                     104
+#define ID_TRAYMENU_OPENMIXER           40001
+#define ID_TRAYMENU_AUDIOPROPERTIES     40002
+#define ID_TRAYMENU_SETTINGS            40003
+#define ID_TRAYMENU_EXIT                40004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40005
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
