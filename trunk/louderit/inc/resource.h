8432eede0d3d2de80bd27925d9cb4e3cdfce3f1a
turbidit <turbidit@9372757c-d666-11dd-9aad-3f7dfcff7c77>
2009-08-30 20:27:22 +0000

    #000 initiated the creation of hotkeys controls on settings dialog

--- trunk/louderit/inc/resource.h
+++ trunk/louderit/inc/resource.h
@@ -3,17 +3,28 @@
 // Used by louderit.rc
 //
 #define IDI_ICON1                       101
-#define IDD_PROPTAB_HK                  102
 #define IDI_ICON2                       103
 #define IDR_MENU1                       104
 #define IDR_CONTEXT                     104
 #define IDD_PROPDLG                     105
+#define IDD_SETTINGS                    105
 #define IDC_TAB                         1001
-#define IDC_CHECK1                      1002
-#define IDC_CHECK2                      1003
-#define IDC_CHECK3                      1004
-#define IDC_CHECK4                      1005
-#define IDC_COMBO1                      1006
+#define IDC_UPCTRLCHECK                 1007
+#define IDC_UPALTCHECK                  1008
+#define IDC_UPSHIFTCHECK                1009
+#define IDC_UPCOMBO                     1010
+#define IDC_UPWINCHECK                  1035
+#define IDC_DOWNCTRLCHECK               1036
+#define IDC_DOWNALTCHECK                1037
+#define IDC_DOWNSHIFTCHECK              1038
+#define IDC_DOWNCOMBO                   1039
+#define IDC_DOWNWINCHECK                1040
+#define IDC_MUTECTRLCHECK2              1046
+#define IDC_MUTECTRLCHECK               1046
+#define IDC_MUTEALTCHECK                1047
+#define IDC_MUTESHIFTCHECK              1048
+#define IDC_WINCOMBO                    1049
+#define IDC_MUTEWINCHECK                1050
 #define ID_TRAYMENU_OPENMIXER           40001
 #define ID_TRAYMENU_AUDIOPROPERTIES     40002
 #define ID_TRAYMENU_SETTINGS            40003
@@ -26,7 +37,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40005
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
